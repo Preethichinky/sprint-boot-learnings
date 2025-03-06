a89a1b4e363bce4be23c091d34407d20af7912d2
Preethichinky <72729258+Preethichinky@users.noreply.github.com>
2025-03-06 23:30:23 +0530

    Add files via upload

---
+++ 04-qualifiers/src/main/java/com/mycode/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.mycode.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 mins in daily practice";
+    }
+}
---
+++ 04-qualifiers/src/main/java/com/mycode/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.mycode.springcoredemo.rest;
+
+
+import com.mycode.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach; //private field for dependency
+
+    //we can give any method name and still it works
+    @Autowired
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach)
+    {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
