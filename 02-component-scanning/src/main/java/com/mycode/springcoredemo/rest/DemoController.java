c14df26a7efe7c7fdac65e3033375f747af29fe5
Preethichinky <72729258+Preethichinky@users.noreply.github.com>
2025-03-06 19:57:22 +0530

    Add files via upload

---
+++ 02-component-scanning/src/main/java/com/mycode/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.mycode.springcoredemo.rest;
+
+
+import com.mycode.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach; //private field for dependency
+
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
