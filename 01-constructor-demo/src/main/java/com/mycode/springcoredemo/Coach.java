ac5955613400ebb19304fa88d1606d33afb95497
Preethichinky <72729258+Preethichinky@users.noreply.github.com>
2025-03-06 18:57:52 +0530

    Add files via upload

---
+++ 01-constructor-demo/src/main/java/com/mycode/springcoredemo/Coach.java
@@ -0,0 +1,5 @@
+package com.mycode.springcoredemo;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ 01-constructor-demo/src/main/java/com/mycode/springcoredemo/DemoController.java
@@ -0,0 +1,23 @@
+package com.mycode.springcoredemo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach; //private field for dependency
+
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
